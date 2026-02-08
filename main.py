2756f26785290318d435f69bbbbf904ebba65755
Swayam-Alhat <swayamalhat@gmail.com>
2026-02-08 21:22:59 +0530

    clearing basics

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import numpy as np
-arr1 = np.array([1,2,3])
-print(type(arr1))
-print(arr1)
+
+# Create a simple array
+arr = np.array([1, 2, 3, 4])
+print(arr)
+print(arr.shape)  # See the shape
+print(arr[0])     # Access first element
+
+arr2d = np.array([[1,2,3],[4,5,6]])
+print(arr2d)
+print(arr2d.shape)
+print(arr2d[0])

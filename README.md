0d7935bd71250b8b17aeb6da71e231ee24ee4e71
Swayam-Alhat <swayamalhat@gmail.com>
2026-02-09 10:20:42 +0530

    learning numpy

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Image Manipulator
+
+`Numpy` is python library to handle & process large data efficiently. It provides functionalities to manipulate data. `Array` is data structure used to store data & allows to perform operations on it.
+
+Data is
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-# Create a simple array
-arr = np.array([1, 2, 3, 4])
-print(arr)
-print(arr.shape)  # See the shape
-print(arr[0])     # Access first element
+arr1 = np.array([[1,2,3],
+                 [4,5,6],
+                 [7,8,9]])
 
-arr2d = np.array([[1,2,3],[4,5,6]])
-print(arr2d)
-print(arr2d.shape)
-print(arr2d[0])
+arr3d = np.array([[[1,2,3],[4,5,6]],
+                  [[7,8,9],[10,11,12]],
+                  [[13,14,15],[16,17,18]]
+                  ])
+print(arr3d.ndim)
+print(arr3d.shape)
+print(arr3d)

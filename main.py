61d2d010f3dd1afaed74a9e5f7282d0dff8ab459
Swayam-Alhat <swayamalhat@gmail.com>
2026-02-14 16:40:33 +0530

    testing pillow

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import numpy as np
+from PIL import Image
 
-# arr1 = np.array([[1,2,3,4,5],
-#                  [6,7,8,9,10]])
-# print(np.sum(arr1))
-# print(np.mean(arr1))
-# print(np.max(arr1))
-# print(np.argmin(arr1))
-# print(np.argmax(arr1))
-# print(arr1)
-# print(np.sum(arr1,axis=1))
-
-rng = np.random.default_rng()
-
-print(rng.integers(1,11,size=(2,2)))
-
+img = Image.open("./images/sun.jpg")
+img.show()

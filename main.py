55e490aa7e2c5be07b8a7533f3389efd32065ce7
Swayam-Alhat <swayamalhat@gmail.com>
2026-02-14 17:11:49 +0530

    testing

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from PIL import Image
 import numpy as np
 
-img = Image.open("./images/sun.jpg")
+img = Image.open("./images/black.jpg")
+arrImg = np.array(img)
+print(arrImg)
+print(arrImg.ndim)
+
+arrImg[:, :, :] = 250
+
+
+img2 = Image.fromarray(arrImg)
+img2.show()

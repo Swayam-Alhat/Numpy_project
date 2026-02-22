db443547caadb5dcdbbee6e32340eb31f982418a
Swayam-Alhat <swayamalhat@gmail.com>
2026-02-22 19:46:05 +0530

    added functions to brighten & darken image

--- color.py
+++ color.py
@@ -1,9 +1,23 @@
-from PIL import Image
 import numpy as np
+from PIL import Image
 
 image = Image.open("./images/nature.jpg")
 
-image_array = np.array(image,dtype=np.int32)
+def brighten_image():
+    # create array with dtype=int32 bcuz 
+    # bydefault np.array convert image into array whose dtype is uint8
+    # uint8 can only hold value from 0 - 255
+    # So when we increase value & if it exceed 255, it starts from 0 again
+    image_array = np.array(image,dtype=np.int32)
+    result_array = image_array + 50
+    np.clip(result_array,0,255,out=result_array)
+    result_image = Image.fromarray(result_array.astype(np.uint8))
+    result_image.show()
 
-print(image_array.ndim)
-print(image_array.shape)
+def darken_image():
+    image_array = np.array(image,dtype=np.int32)
+    result_array = image_array - 50
+    np.clip(result_array,0,255,out=result_array)
+    result_image = Image.fromarray(result_array.astype(np.uint8))
+    result_image.show()
+darken_image()
